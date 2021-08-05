6e5dc72a869ba1e3e27be6adbf9d5969e3903164
Jakob Holst <jho@miracle.dk>
2021-08-05 16:30:03 +0200

    terr

---
+++ terraform/Readme.libvirt.md
@@ -0,0 +1,4 @@
+git clone https://github.com/dmacvicar/terraform-provider-libvirt.git
+cd terraform-provider-libvirt
+make
+
---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "libvirt_disk_path" {
+  description = "path for libvirt pool"
+  default     = "/opt/kvm/pool1"
+}
+
+variable "centos_8_img_url" {
+  description = "centos 8 image"
+  default     = "/home/jho/iso/CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64.qcow2"
+}
+
+variable "vm_hostname" {
+  description = "vm hostname"
+  default     = "terraform-kvm-centos"
+}
+
+variable "ssh_username" {
+  description = "the ssh user to use"
+  default     = "cloud-user"
+}
+
+variable "ssh_private_key" {
+  description = "the private key to use"
+  default     = "~/.ssh/id_rsa"
+}
