2ca9ee5da9b3df76c520bfc743854e5fc5cf0d0d
Jakob Holst <jho@miracle.dk>
2023-06-12 09:23:35 -0200

    demo

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install ansible
-sudo ansible-galaxy collection install ansible.posix
+ansible-galaxy collection install ansible.posix
 ansible-galaxy role install -fr roles/requirements.yml
 
