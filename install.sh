3e3f69032e273735cc411a1736cdf2cb198c08cb
Jakob Holst <jho@miracle.dk>
2023-06-13 14:50:47 +0200

    --

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install ansible
+pip install virtualenv
 ansible-galaxy collection install --force ansible.posix
 ansible-galaxy role install -fr roles/requirements.yml
 ansible-playbook awxrpm.yml
