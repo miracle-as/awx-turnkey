dd218044cb31fed2d30639962a5e685fb64db953
Jakob Holst <jho@miracle.dk>
2023-06-13 13:53:01 +0200

    --

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ pip install --upgrade pip
 pip install ansible
 ansible-galaxy collection install --force ansible.posix
 ansible-galaxy role install -fr roles/requirements.yml
+ansible-playbook awxrpm.yml
 
