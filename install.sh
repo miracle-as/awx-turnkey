61fc858d3ae1e93a1ef4f9ccee79ff3126517a6a
Jakob Holst <jho@miracle.dk>
2023-06-16 07:13:35 +0200

    pretty istall

--- install.sh
+++ install.sh
@@ -63,6 +63,8 @@ source /usr/local/venv/bin/activate
 pip install --upgrade pip
 pip install ansible
 pip install virtualenv
+pip install uwsgi
+
 # Install Ansible AWX
 ansible-galaxy collection install --force ansible.posix
 ansible-galaxy role install -fr roles/requirements.yml
