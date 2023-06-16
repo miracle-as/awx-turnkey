a689c7644c8586d1e1f175ec67366b811119e4fe
Jakob Holst <jho@miracle.dk>
2023-06-16 06:16:26 +0200

    comments

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 python3 -m venv /usr/local/venv
+sudo subscription-manager repos --enable codeready-builder-for-rhel-9-x86_64-rpms
 
+# Install Ansible   
 source /usr/local/venv/bin/activate
 pip install --upgrade pip
 pip install ansible
+pip install ansible-lint
 pip install virtualenv
-pip install yolk3k
+# Install Ansible AWX
 ansible-galaxy collection install --force ansible.posix
 ansible-galaxy role install -fr roles/requirements.yml
 ansible-playbook awxrpm.yml
